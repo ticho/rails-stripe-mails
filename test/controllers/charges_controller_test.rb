2f49e59664cfc3519c9d20fe86b1552c804917d0
ticho <tibofolly@gmail.com>
2018-08-09 22:45:01 +0200

    test

--- test/controllers/charges_controller_test.rb
+++ test/controllers/charges_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ChargesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "payment button is present" do
+    get root_path
+    assert_select 'form'
+
+  end
+
+  test "welcome msg is visible" do
+    get root_path
+    assert_select 'h1', 'Welcome to my website !'
+  end
 end
